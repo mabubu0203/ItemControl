f2184902772c76f5aafea9e7adc94bd1fcb87309
uratamanabu <mabu_ip@msn.com>
2018-02-24 21:05:02 +0900

    １件取得追加

--- micro-api/src/main/java/jp/co/valtech/items/api/category/controller/CategoryController.java
+++ micro-api/src/main/java/jp/co/valtech/items/api/category/controller/CategoryController.java
@@ -2,20 +2,26 @@ package jp.co.valtech.items.api.category.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import jp.co.valtech.items.api.category.helper.CategoryCreateHelper;
+import jp.co.valtech.items.api.category.helper.CategoryFindHelper;
 import jp.co.valtech.items.api.category.helper.CategoryGetHelper;
 import jp.co.valtech.items.common.exception.ConflictException;
+import jp.co.valtech.items.common.exception.NotFoundException;
 import jp.co.valtech.items.interfaces.category.requests.CategoryCreateRequest;
 import jp.co.valtech.items.interfaces.category.responses.CategoryCreateResponse;
+import jp.co.valtech.items.interfaces.category.responses.CategoryFindResponse;
 import jp.co.valtech.items.interfaces.category.responses.CategoryGetResponse;
 import jp.co.valtech.items.interfaces.definitions.responses.ErrorRes;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,6 +39,7 @@ import javax.validation.Valid;
 @Api(description = "カテゴリーを扱います。")
 public class CategoryController {
     private final CategoryCreateHelper create;
+    private final CategoryFindHelper find;
     private final CategoryGetHelper get;
 
     /**
@@ -60,6 +67,34 @@ public class CategoryController {
 
         log.info("create");
         return create.execute(request);
+
+    }
+
+    /**
+     * カテゴリーを1件取得します。
+     *
+     * @author uratamanabu
+     * @version 1.0
+     * @since 1.0
+     */
+    @GetMapping(value = {"/{id}"})
+    @ApiOperation(
+            value = "${CategoryController.findCategory.value}",
+            notes = "${CategoryController.findCategory.notes}"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 200, message = "", response = CategoryFindResponse.class),
+                    @ApiResponse(code = 400, message = "", response = ErrorRes.class),
+                    @ApiResponse(code = 404, message = "", response = ErrorRes.class)
+            }
+    )
+    public ResponseEntity<CategoryFindResponse> findGoods(
+            @PathVariable(name = "id") @Range(min = 0, max = 999999999) @ApiParam(example = "1", value = "${CategoryController.findCategory.request.id.value}") final long id
+    ) throws NotFoundException {
+
+        log.info("find");
+        return find.execute(id);
 
     }
 
---
+++ micro-api/src/main/java/jp/co/valtech/items/api/category/helper/CategoryFindHelper.java
@@ -0,0 +1,39 @@
+package jp.co.valtech.items.api.category.helper;
+
+import jp.co.valtech.items.common.exception.NotFoundException;
+import jp.co.valtech.items.interfaces.category.responses.CategoryFindResponse;
+import jp.co.valtech.items.interfaces.definitions.responses.CategoryRes;
+import jp.co.valtech.items.rdb.domain.CategoryTbl;
+import jp.co.valtech.items.rdb.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CategoryFindHelper {
+
+    private final CategoryService service;
+    private final ModelMapper modelMapper;
+
+    public ResponseEntity<CategoryFindResponse> execute(
+            final long id
+    ) throws NotFoundException {
+
+        Optional<CategoryTbl> optionalId = service.findById(id);
+        CategoryTbl entity = optionalId
+                .orElseThrow(() -> new NotFoundException("id", "IDが存在しません。"));
+        CategoryRes categoryRes = modelMapper.map(entity, CategoryRes.class);
+        modelMapper.map(entity.getStatusTbl(), categoryRes);
+        CategoryFindResponse response = new CategoryFindResponse();
+        response.setCategory(categoryRes);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+
+    }
+}
