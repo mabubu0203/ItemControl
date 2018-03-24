c09f85706f4f33fe2e3e5c70b2745ef3e4f75fa2
uratamanabu <mabu_ip@msn.com>
2018-03-25 01:04:49 +0900

    micro-rdbのテストソース追加

--- micro-rdb/src/test/java/jp/co/valtech/items/rdb/repository/GoodsRepositoryTest.java
+++ micro-rdb/src/test/java/jp/co/valtech/items/rdb/repository/GoodsRepositoryTest.java
@@ -14,7 +14,10 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
+import javax.persistence.NoResultException;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
@@ -29,23 +32,86 @@ class GoodsRepositoryTest {
     private GoodsRepository repository;
 
     @Nested
-    @DisplayName("登録")
+    @DisplayName("GoodsCode検索")
     class findByCodeTest {
 
         @FlywayTest
         @Test
-        void find1() throws Exception {
+        void findSuccess() throws Exception {
 
             String code = "CODEG1";
             Optional<GoodsTbl> opt = repository.findByCode(code);
             GoodsTbl entity = opt.get();
+
             Assertions.assertEquals(entity.getCode(), code);
+            Assertions.assertEquals(entity.getCategoryId(), Long.valueOf("1"));
             Assertions.assertEquals(entity.getName(), "GET");
+            Assertions.assertEquals(entity.getPrice(), Integer.valueOf("1"));
+            Assertions.assertEquals(entity.getNote(), "NOTE");
 
         }
 
+        @FlywayTest
+        @Test
+        void findFail() throws Exception {
+
+            String code = "CODEG100";
+            Optional<GoodsTbl> opt = repository.findByCode(code);
+            Assertions.assertThrows(NoSuchElementException.class, () -> opt.get());
+
+        }
 
     }
 
+    @Nested
+    @DisplayName("GoodsId検索")
+    class findByIdJoinStatusTest {
+
+        @FlywayTest
+        @Test
+        void findSuccess() throws Exception {
+
+            Long id = Long.valueOf("1");
+            GoodsTbl entity = repository.findByIdJoinStatus(id);
+
+            Assertions.assertEquals(entity.getCode(), "CODEG1");
+            Assertions.assertEquals(entity.getCategoryId(), Long.valueOf("1"));
+            Assertions.assertEquals(entity.getName(), "GET");
+            Assertions.assertEquals(entity.getPrice(), Integer.valueOf("1"));
+            Assertions.assertEquals(entity.getNote(), "NOTE");
+
+        }
+
+        @FlywayTest
+        @Test
+        void findFail() throws Exception {
+
+            Long id = Long.valueOf("100");
+            Assertions.assertThrows(NoResultException.class, () -> repository.findByIdJoinStatus(id));
+
+        }
+
+    }
+
+    @Nested
+    @DisplayName("全検索")
+    class getAllJoinStatusTest {
+
+        @FlywayTest
+        @Test
+        void findSuccess() throws Exception {
+
+            Stream<GoodsTbl> stream = repository.getAllJoinStatus();
+            Assertions.assertEquals(stream.count(), 5);
+
+        }
+
+    }
+
+    @Nested
+    @DisplayName("条件検索")
+    class searchJoinStatusTest {
+
+    }
 
 }
